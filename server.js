79daad9e6813a6fb477f6a6ada5705dc8619ad4d
Noa Midzic <nmidzic@yahoo.com>
2023-08-11 12:15:59 +0200

    Removed index.html, added dynamic HTML pages, moved game files to separate directory

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const sqlite3 = require("sqlite3");
+const fs = require("fs/promises");
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const db = new sqlite3.Database("game_data.db");
@@ -15,6 +17,20 @@ db.run(`
     score INTEGER
   )
 `);
+
+app.get("/", (req, res) => {
+  res.redirect("/game");
+});
+
+app.get("/game", async (req, res) => {
+  const gameHtml = await loadPage("game");
+  res.send(gameHtml);
+});
+
+app.get("/leaderboard", async (req, res) => {
+  const leaderboardHtml = await loadPage("leaderboard");
+  res.send(leaderboardHtml);
+});
 
 app.post("/save_score", (req, res) => {
   const { score } = req.body;
@@ -33,3 +49,14 @@ app.post("/save_score", (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+async function loadPage(pageTitle) {
+  let pages = [loadHTML(pageTitle), loadHTML("nav")];
+  let [page, nav] = await Promise.all(pages);
+  page = page.replace("#nav#", nav);
+  return page;
+}
+
+function loadHTML(path) {
+  return fs.readFile(__dirname + "/public/html/" + path + ".html", "UTF-8");
+}
